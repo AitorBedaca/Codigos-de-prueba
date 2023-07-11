6402506a756d0f759d61291609542316cfe89f89
AitorBedaca <126891738+AitorBedaca@users.noreply.github.com>
2023-07-11 11:05:27 -0300

    Update Prueba_Joystick.ino

--- Prueba_Joystick/Prueba_Joystick.ino
+++ Prueba_Joystick/Prueba_Joystick.ino
@@ -1,5 +1,5 @@
-#define EJE_X 14 //A0
-#define EJE_Y 15 //A1
+#define EJE_X 62 //A8
+#define EJE_Y 63 //A9
 #define BOTON 2
 
 void setup() {
@@ -12,8 +12,8 @@ void loop() {
   int valorX = analogRead(EJE_X),
       valorY = analogRead(EJE_Y),
       botonPulsado = digitalRead(BOTON);
-   //Serial.print("\nValor X: ");
-    //Serial.print(valorX);
+    Serial.print("\nValor X: ");
+    Serial.print(valorX);
     Serial.print("\nValor Y: ");
     Serial.print(valorY);
   if (botonPulsado == 1){
